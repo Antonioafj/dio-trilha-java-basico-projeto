827df272987ea7fbf8b1fe9871a175a36b4a8359
Antonioafj <antonioafj.edu@gmail.com>
2024-05-01 20:28:35 -0400

    configurado os campos de insercao de dados pelo usuario

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("\nPor favor ensira o numero da agência:");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("\nAgora digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+
+        scanner.nextLine();
+        System.out.println("\nDigite seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("\nDigite o saldo:");
+        Double saldo = scanner.nextDouble();
+
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é a " + numeroAgencia + ",\ncom numero de conta  " + numeroConta + ", e seu saldo de " + saldo + " já está disponível.\n ");
     }
 }
